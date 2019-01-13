f6cee3efff91bf0d3cd115e7d8bed26e4f27eaba
Jaroca124 <jacob.caccamo@gmail.com>
2019-01-13 16:53:52 -0500

    Adding Movement to Player

---
+++ Drone Wars/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float lookSensitivity;
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float _xMov = Input.GetAxisRaw("LeftStickX");
+        float _zMov = Input.GetAxisRaw("LeftStickY");
+
+        Vector3 _moveHorizontal = transform.right * _xMov;
+        Vector3 _moveVertical = transform.forward * _zMov;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        // Rotation
+
+        float _yRot = Input.GetAxisRaw("RightStickX");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("RightStickY");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+    }
+
+}
---
+++ Drone Wars/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    public Camera cam;
+
+    private Rigidbody drone;
+
+    void Start()
+    {
+        drone = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            drone.MovePosition(drone.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation()
+    {
+        drone.MoveRotation(drone.rotation * Quaternion.Euler(rotation));
+        if (cam != null) {
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+}
